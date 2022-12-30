7966669027e65330ec8c588530bd63f405384d56
ascensom <1210855@isep.ipp.pt>
2022-12-30 10:49:43 +0000

    [ARQCP] Restore ignored files
    (cherry picked from commit b2d3e2504bfb459d722912031c2fb707925a10c0)

---
+++ ARQCP/sensor/sensor.c
@@ -0,0 +1,72 @@
+#include "sensor.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+unsigned long read_frequency_from_config_properties(unsigned char *sensor_type) {
+    // get the frequency from the config.properties file
+
+    unsigned long frequency;
+
+    char line[100];  // buffer to hold a line of text from the file
+    FILE* fp;
+
+    fp = fopen("../config.properties", "r");
+    
+    if (fp == NULL) {
+        printf("Error opening file");
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        if (sscanf(line, "frequency_sensor_%c%c = %lu", &sensor_type[0], &sensor_type[1], &frequency) == 1) {
+            // frequency variable was found and read successfully
+            break;
+        }
+    }
+
+    fclose(fp);
+
+    return frequency;  // return the value of the frequency variable
+};
+
+int define_readings_size(Sensor *sensor) {
+
+    unsigned long frequency = read_frequency_from_config_properties(sensor->sensor_type);
+
+    if (frequency == 0) {
+        printf("Error reading frequency from config.properties file");
+        return 1;
+    }
+
+    sensor->readings_size = 86400 / frequency;  // 86400 seconds in a day
+
+    // Reallocate memory for the unsigned short *readings array;
+    sensor->readings = (unsigned short *) realloc(sensor->readings, sensor->readings_size * sizeof(unsigned short));
+
+    return 0;
+};
+
+
+//Function to create a sensor
+Sensor *create_sensor(unsigned short id, unsigned char sensor_type[2]){
+    Sensor *sensor = (Sensor*)malloc(sizeof(Sensor));
+    sensor->id = id;
+    sensor->sensor_type[0] = sensor_type[0];
+    sensor->sensor_type[1] = sensor_type[1];
+    sensor->max_limit = 0;
+    sensor->min_limit = 0;
+    sensor->frequency = 0;
+    sensor->readings_size = 0;
+    sensor->readings = 0;
+    return sensor;
+}; // By now
+
+//Function to destroy (free) a sensor
+void destroy_sensor(Sensor *sensor){
+    free(sensor);
+};
+
+void print_sensor(Sensor *sensor){
+    printf("Sensor ID: %d | Sensor Type: %s\n", sensor->id, sensor->sensor_type);
+};
---
+++ ARQCP/sensor/sensor.h
@@ -0,0 +1,50 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+typedef struct
+{
+    unsigned short id;
+    unsigned char sensor_type[2];
+    // limites do sensor
+    unsigned short max_limit;
+    unsigned short min_limit;
+    // frequency de leituras (em segundos)
+    unsigned long frequency;
+    // tamanho do array de leituras
+    unsigned long readings_size;
+    // array de leituras diárias 
+    unsigned short *readings;
+    //...
+    // adicionar o que acharem conveniente
+} Sensor;
+
+//[id][id][id][id] [id][id][id][id]  // short: 2 bytes
+//[st][st][][] [][] [][] [][] [][]  // ptr: 2 bytes
+//[max][max][max][max] [max][max][max][max]  // short: 2 bytes
+//[min][min][min][min] [min][min][min][min]  // short: 2 bytes
+//[freq][freq][freq][freq] [freq][freq][freq][freq] [freq][freq][freq][freq] [freq][freq][freq][freq] // long: 4 bytes
+//[rs][rs][rs][rs] [rs][rs][rs][rs] [rs][rs][rs][rs] [rs][rs][rs][rs] // long: 4 bytes
+//[r][r][r][r] [r][r][r][r]  // ptr: 2 bytes
+
+/**
+    * @brief Define *readings size.
+    * The size is defined by the daily frequency.
+    * The frequency is in seconds, and its readed by the function.
+    * The number of seconds in a day is 86400.
+    * The size is defined by the formula: 86400 / frequency.
+    * @param sensor Sensor to be defined.
+    * @return 0 if the size was defined, -1 if the size was not defined.
+    */
+int define_readings_size(Sensor *sensor);
+
+//Function to create a sensor
+Sensor *create_sensor(unsigned short id, unsigned char sensor_type[2]); // By now
+
+//Function to destroy (free) a sensor
+void destroy_sensor(Sensor *sensor);
+
+unsigned long read_frequency_from_config_properties(unsigned char *sensor_type);
+
+void print_sensor(Sensor *sensor);
+
+#endif
