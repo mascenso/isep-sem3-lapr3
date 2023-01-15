9e77102b21278e3042941739a0dbfa9bae44a696
ascensom <1210855@isep.ipp.pt>
2023-01-15 17:20:02 +0000

    Delete unused files

--- ARQCP/US101/demo_pcg.h
+++
@@ -1,6 +0,0 @@
-#ifndef DEMO_PCG_H
-#define DEMO_PCG_H
-
-	unsigned long long pcg32_random_r();
-
-#endif
--- ARQCP/US101/read_rnd.h
+++
@@ -1,5 +0,0 @@
-#ifndef READ_RND_H
-#define READ_RND_H
-unsigned long long  read_rnd();
-
-#endif
